895e5a6b5e7cda531239ae45e7fd4f8dfa51effd
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-02-18 01:00:13 -0300

    exe factorial

---
+++ student/src/created/FactorialExercise.java
@@ -0,0 +1,16 @@
+package created;
+
+public class FactorialExercise {
+	
+	public static void main(String[] args) {
+		
+		int factorial = 1;
+		
+		for (int n = 1; n < 11; n++) {
+			factorial *= n;	
+			System.out.println("Factorial de " + n + " = "+ factorial);
+		
+		}
+	}
+
+}
