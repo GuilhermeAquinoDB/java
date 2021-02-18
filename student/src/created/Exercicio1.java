46e872817932d583b0aa39400a26955a1c2fb38c
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-02-17 23:40:18 -0300

    while and for

---
+++ student/src/created/Exercicio1.java
@@ -0,0 +1,17 @@
+package created;
+
+// Utilize um laço for para imprimir todos os múltiplos de 3, entre 1 e 100.
+
+public class Exercicio1 {
+	
+	public static void main(String[] args) {
+		
+		
+		for (int i = 1; i < 100; i++) {
+			if (i % 3 == 0)
+			System.out.println(i);
+		}
+		
+	}
+
+}
