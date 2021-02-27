55539193982c5fdd79228065614f5ddc187f9958
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-02-26 23:59:11 -0300

    second instance

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -5,6 +5,13 @@ public class CriaConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
-			
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println("A segunda conta tem " + segundaConta.saldo);
+		
 	}
 }
