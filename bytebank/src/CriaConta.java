0277823115b6244d4c8beffb7aa969c8cd1af9ce
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-02-27 01:05:06 -0300

    Test References

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -9,9 +9,26 @@ public class CriaConta {
 		primeiraConta.saldo += 100;
 		System.out.println(primeiraConta.saldo);
 		
+		
 		Conta segundaConta = new Conta();
 		segundaConta.saldo = 50;
 		System.out.println("A segunda conta tem " + segundaConta.saldo);
 		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		System.out.println(segundaConta.agencia);
+		
+		segundaConta.agencia = 146;
+		System.out.println("agora a segunda conta na agencia " + segundaConta.agencia);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("mesma conta");
+		} else {
+			System.out.println("contas diferentes");
+		}
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
 	}
 }
