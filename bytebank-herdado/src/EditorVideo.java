03879d35f75dc55c232f35f4f0dffc277ffef39c
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-03-06 20:40:07 -0300

    Polymorphism

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,9 @@
+//EditoVideo herda da class Funcionario
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Editor de Video");
+		return super.getBonificacao() + 150;
+	}
+}
+	
