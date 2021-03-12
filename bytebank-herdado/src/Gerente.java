b0b8c1b263b901e9399b5ba463bb5ca0860ec037
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-03-11 22:18:06 -0300

    more abstract class

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,19 +1,5 @@
-//Gerente herda da class Funcionario
-public class Gerente extends Funcionario {
-
-	private int senha;
-
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-
-	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+//Gerente herda da class FuncionarioAutenticavel
+public class Gerente extends FuncionarioAutenticavel {
 
 	public double getBonificacao() {
 		System.out.println("Chamando o método de bonificacao do Gerente");
---
+++ bytebank-herdado/src/SistemaInterno.java
@@ -0,0 +1,16 @@
+
+public class SistemaInterno {
+
+	private int senha = 222;
+	
+	public void autentica(FuncionarioAutenticavel fa) {
+		boolean autenticou = fa.autentica(this.senha);
+		if (autenticou) {
+			System.out.println("Pode entrar no Sistema");
+		} else {
+			System.out.println("Não possui permissão para entrar");
+		}
+
+	}
+
+}
