8da39f6cd96dc1acdc89fc5edd64ef396fc841fe
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-03-14 21:57:21 -0300

    Another Interface

---
+++ bytebank-herdado-conta/src/TestTributaveis.java
@@ -0,0 +1,17 @@
+
+public class TestTributaveis {
+
+	public static void main(String[] args) {
+		ContaCorrente cc = new ContaCorrente(123, 456);
+		cc.deposita(100.0);
+		
+		SeguroDeVida seguro = new SeguroDeVida();
+		
+		
+		CalculadorImposto calc = new CalculadorImposto();
+		calc.registra(cc);
+		calc.registra(seguro);
+		
+		System.out.println(calc.getTotalImposto());
+	}
+}
