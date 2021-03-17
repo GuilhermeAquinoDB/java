4b3a5d96a16fba06ee7b0744e283678c14ee52a7
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-03-16 21:58:55 -0300

    Catch

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -4,8 +4,10 @@ public class Fluxo {
 		System.out.println("Ini do main");
 		try {
 			metodo1();
-		} catch (ArithmeticException ex) {
-			System.out.println("ArithmeticExcepetion");
+		} catch (ArithmeticException | NullPointerException ex) {
+			String msg = ex.getMessage();
+			System.out.println("Excepetion " + msg);
+			ex.printStackTrace();
 		}
 		System.out.println("Fim do main");
 	}
@@ -20,7 +22,9 @@ public class Fluxo {
 		System.out.println("Ini do metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
-				int a = i / 0;
+//				int a = i / 0;
+			Conta c = null;
+			c.saque();
 		}
 		System.out.println("Fim do metodo2");
 	}
