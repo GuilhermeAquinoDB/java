786205518b661465c460da69ef7d94d0ad695f90
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-04-28 01:06:48 -0300

    Class Integer

--- bytebank-herdado-conta/src/br/com/bytebank/bank/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/bank/test/util/Teste.java
@@ -6,14 +6,26 @@ import java.util.List;
 public class Teste {
 
 	public static void main(String[] args) {
+
+		int idade = 29;// Integer
+
+		Integer idadeRef = Integer.valueOf(29); // autoboxing
+		System.out.println(idadeRef.doubleValue());
 		
-		int[] idades = new int[5];
-		String[] nomes = new String[5];
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
 		
-		int idade = 29;//Integer
-		Integer idadeRef = new Integer(29);
+		System.out.println(Integer.SIZE);
+		System.out.println(Integer.BYTES);
+
+		int valor = idadeRef.intValue(); // unboxing
+		String s = args[0];// "10"
+		// Integer numero = Integer.valueOf(s);
+		int numero = Integer.parseInt(s);
+		System.out.println(numero);
+
 		List<Integer> numeros = new ArrayList<Integer>();
-		numeros.add(29); //Autoboxing
+		numeros.add(29); // Autoboxing
 
 	}
 
