20beb0c78958a9fe59bdc5764fd5741cf84064bb
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-05-22 22:58:10 -0300

    Writer

---
+++ java-io/src/br/com/student/io/test/TesteEscrita.java
@@ -0,0 +1,27 @@
+package br.com.student.io.test;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita {
+
+	public static void main(String[] args) throws IOException {
+
+		// Fluxo de Saida com Arquivo
+		OutputStream fos = new FileOutputStream("lorem2.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		bw.write("Lorem impsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+//		bw.newLine();
+		bw.newLine();
+		bw.write("fghjnfjhngfvnjgfnjhgfnjghfyjnghyjnjnvfgnj");
+		
+		bw.close();
+	}
+
+}
