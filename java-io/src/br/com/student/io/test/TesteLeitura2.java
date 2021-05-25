f0114450ce4844f9d987e43f6f5beed8b8314d2d
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-05-24 23:48:12 -0300

    Scanner

---
+++ java-io/src/br/com/student/io/test/TesteLeitura2.java
@@ -0,0 +1,20 @@
+package br.com.student.io.test;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner scanner = new Scanner(new File("contas.csv"));
+
+		while (scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+			System.out.println(linha);
+
+		}
+		scanner.close();
+	}
+
+}
