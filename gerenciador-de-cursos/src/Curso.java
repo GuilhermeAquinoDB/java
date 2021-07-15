23c40c0edfbb2d25c49ac4a6ce8c515cc9682d64
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-07-15 14:52:49 -0300

    Implementando Set no modelo

--- gerenciador-de-cursos/src/Curso.java
+++ gerenciador-de-cursos/src/Curso.java
@@ -1,12 +1,15 @@
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 
@@ -46,6 +49,15 @@ public class Curso {
 	public String toString() {
 
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + "," + " aulas: " + this.aulas + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
 	}
 
 }
