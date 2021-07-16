379944d24eabe20ab1293598dfa217bfd8decaa3
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-07-16 20:06:39 -0300

    Lambdas

--- java8/src/java8/OrdenaStrings.java
+++ java8/src/java8/OrdenaStrings.java
@@ -16,48 +16,22 @@ public class OrdenaStrings {
 		palavras.add("Editora casa do codigo");
 		palavras.add("Caelum");
 
-		Comparator<String> comparador = new ComparadorPorTamanho();
+		palavras.sort((s1, s2) -> {
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
+			return 0;
+		});
 
-		palavras.sort(comparador);
-		
-		// Collections.sort(palavras, comparador);
-		
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
 		System.out.println(palavras);
 
-		/*
-		 * for (String p : palavras) { System.out.println(p); }
-		 */
+//		Consumer<String> impressor = s -> System.out.println(s);
+//		palavras.forEach(impressor);
 		
-		// imprimindo cada String com o novo método default (forEach)
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		palavras.forEach(s -> System.out.println(s));
 
 	}
-
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-
-	}
-
-}
-
-// Ordene essa lista de Strings utilizando o tamanho da String como critério de comparação.
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
-
 }
