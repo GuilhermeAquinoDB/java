9a69132cc94ba7cce25f49f96ad6f9a8a895e538
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-07-17 10:40:42 -0300

    Streams, Collectors e APIs

--- java8/src/java8/ExemploCursos.java
+++ java8/src/java8/ExemploCursos.java
@@ -3,6 +3,9 @@ package java8;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 class Curso {
 
@@ -37,19 +40,32 @@ public class ExemploCursos {
 
 		// lambda
 		cursos.sort(Comparator.comparingInt(c -> c.getAlunos()));
-		//Method reference
-		//cursos.sort(Comparator.comparingInt(Curso::getAlunos));
-		// cursos.forEach(c -> System.out.println(c.getNome()));
 
-		int sum = cursos.stream()
+//		int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
+
+//		System.out.println(sum);
+
+//		cursos.stream()
+//				.filter(c -> c.getAlunos() >= 100)
+//				.findAny()
+//				.ifPresent(c -> System.out.println(c.getNome()));
+//		
+//		cursos = cursos.stream()
+//		.filter(c -> c.getAlunos() >= 100)
+//		.collect(Collectors.toList());
+//		
+		cursos.stream()
 			.filter(c -> c.getAlunos() >= 100)
-			.mapToInt(Curso::getAlunos)
-			.sum();
+			.collect(Collectors.toMap(
+					c -> c.getNome(),
+					c -> c.getAlunos()))
+			.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
 		
-		System.out.println(sum);
 		
-			//.forEach(c -> System.out.println(c.getNome()));
-			//.map(Curso::getAlunos)
-			//.forEach(System.out::println);
+//		cursos.stream().forEach(c -> System.out.println(c.getNome()));
+		
+//		Curso curso = optionalCurso.orElseThrow(null);
+//		System.out.println(curso.getNome());
+
 	}
 }
